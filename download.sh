ac34f80e10cb7cc53b1b6288e73a2ff207175534
Salvador E. Tropea <salvador@inti.gob.ar>
2020-06-27 13:06:20 -0300

    Updated IBoM and added the dependencies for the next kiplot release.

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm *.deb
+curl -s https://api.github.com/repos/set-soft/mcpy/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
+curl -s https://api.github.com/repos/set-soft/macropy/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
 curl -s https://api.github.com/repos/INTI-CMNB/kicad-automation-scripts/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
 curl -s https://api.github.com/repos/INTI-CMNB/InteractiveHtmlBom/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
 curl -s https://api.github.com/repos/INTI-CMNB/kiplot/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -i -
