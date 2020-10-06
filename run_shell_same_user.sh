0475d2562bdf638649092814fa03a1c8bd2534c0
Salvador E. Tropea <salvador@inti.gob.ar>
2020-10-06 17:55:00 -0300

    Added script to run a shell in the container.

---
+++ run_shell_same_user.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+    --user $USER_ID:$GROUP_ID \
+    --env NO_AT_BRIDGE=1 \
+    --workdir="/home/$USER" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/home/$USER:/home/$USER:rw" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/home/$USER:/home/$USER:rw" \
+    setsoft/kicad_auto:latest /bin/bash
