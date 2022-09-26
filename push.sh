9711d39acc39deb45a3358cc011e0e75c5400649
Salvador E. Tropea <salvador@inti.gob.ar>
2022-09-26 13:54:28 -0300

    Updated the push script (now generated)

--- push.sh
+++ push.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+docker push setsoft/kicad_debian:11.5-5.1.9
+docker push setsoft/kicad_debian:ki6.0.7_Debian
 docker push setsoft/kicad_auto:11.5-5.1.9
 docker push setsoft/kicad_auto:ki6.0.7_Debian
 docker push setsoft/kicad_auto_test:11.5-5.1.9
 docker push setsoft/kicad_auto_test:ki6.0.7_Debian
+docker push ghcr.io/inti-cmnb/kicad_debian:11.5-5.1.9
+docker push ghcr.io/inti-cmnb/kicad_debian:ki6.0.7_Debian
+docker push ghcr.io/inti-cmnb/kicad_auto:11.5-5.1.9
+docker push ghcr.io/inti-cmnb/kicad_auto:ki6.0.7_Debian
+docker push ghcr.io/inti-cmnb/kicad_auto_test:11.5-5.1.9
+docker push ghcr.io/inti-cmnb/kicad_auto_test:ki6.0.7_Debian
