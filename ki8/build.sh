7ae22b07b83dc85249be72e08d99a0d6c1b30b97
Salvador E. Tropea <salvador@inti.gob.ar>
2024-06-19 10:38:23 -0300

    Fixed tag for KiCad 8
    No longer using Sid

--- ki8/build.sh
+++ ki8/build.sh
@@ -4,7 +4,7 @@ set -e
 docker build -f Dockerfile --build-arg iteration=${IT} -t ghcr.io/inti-cmnb/kicad8_auto:latest .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:latest kibot --version | sed 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\(\.[0-9]\+\)\?\) .*/\1/' | tr -d '\n'`
 TG2=k`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:latest kicad_version.py`
-TG3=d_sid
+TG3=d`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:latest cat /etc/debian_version | tr -d '\n'`
 docker tag ghcr.io/inti-cmnb/kicad8_auto:latest ghcr.io/inti-cmnb/kicad8_auto:${TG1}-${IT}_${TG2}_${TG3}
 docker tag ghcr.io/inti-cmnb/kicad8_auto:latest ghcr.io/inti-cmnb/kicad8_auto:${TG1}
 docker tag ghcr.io/inti-cmnb/kicad8_auto:latest ghcr.io/inti-cmnb/kicad_auto:ki8
--- ki8_pre/build.sh
+++ ki8_pre/build.sh
@@ -3,7 +3,7 @@ set -e
 cp ../dl_deb.py .
 docker build -f Dockerfile -t ghcr.io/inti-cmnb/kicad8_auto:latest_deps .
 TG1=`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:latest_deps kicad_version.py`
-TG2=d_sid
+TG2=d`docker run --rm ghcr.io/inti-cmnb/kicad8_auto:latest_deps cat /etc/debian_version | tr -d '\n'`
 docker tag ghcr.io/inti-cmnb/kicad8_auto:latest_deps ghcr.io/inti-cmnb/kicad8_auto:${TG1}_${TG2}_deps
 docker push ghcr.io/inti-cmnb/kicad8_auto:${TG1}_${TG2}_deps
 docker push ghcr.io/inti-cmnb/kicad8_auto:latest_deps
